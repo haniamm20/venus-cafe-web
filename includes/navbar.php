31138c3826b8df7e5febaa1d02ecb874e4ecf32b
haniamm20 <haniamm20@gmail.com>
2026-07-21 17:24:34 +0700

    Add navbar component to includes/navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top shadow">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="index.php">
+            <img src="assets/images/banner.png" width="45" class="me-2 rounded-circle">
+            VENUS CAFE
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu">
+
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Menu</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Booking</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontak</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="btn btn-warning ms-2" href="login.php">
+                        Login
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
